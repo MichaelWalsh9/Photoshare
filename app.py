71cd43d34c816a7eb8f682dab290677d6857cd40
Michael Walsh <mpw9@bu.edu>
2023-02-27 11:44:24 -0500

    Reformatted app.py, added generalized database query functions

--- app.py
+++ app.py
@@ -7,7 +7,7 @@
 # and MaxCountryMan at https://github.com/maxcountryman/flask-login/
 # and Flask Offical Tutorial at  http://flask.pocoo.org/docs/0.10/patterns/fileuploads/
 # see links for further understanding
-###################################################
+############################################################################################################################################
 
 import flask
 from flask import Flask, Response, request, render_template, redirect, url_for
@@ -31,7 +31,10 @@ app.config['MYSQL_DATABASE_DB'] = 'photoshare'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-#begin code used for login
+############################################################################################################################################
+#	    LOGIN CODE		 ###################################################################################################################
+############################################################################################################################################
+
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
@@ -72,12 +75,106 @@ def request_loader(request):
 	user.is_authenticated = sha256(request.form['password']) == pwd
 	return user
 
-'''
-A new page looks like this:
-@app.route('new_page_name')
-def new_page_function():
-	return new_page_html
-'''
+
+############################################################################################################################################
+# HELPER FUNCTIONS FOR GETTING ATTRIBUTES AND OTHER INFO ###################################################################################
+############################################################################################################################################
+
+def getCurrentUid():
+	return getUserIdFromEmail(flask_login.current_user.id)
+
+def getUsersPhotos(uid):
+	cursor = conn.cursor()
+	cursor.execute("SELECT imgdata, picture_id, caption FROM Pictures WHERE user_id = '{0}'".format(uid))
+	return cursor.fetchall() #NOTE return a list of tuples, [(imgdata, pid, caption), ...]
+
+# General purpose function that returns all attributes of a specified type in a table relation to a key
+def getAttributesByKey(table, key, attribute, id):
+	cursor = conn.cursor()
+	cursor.execute("SELECT {0} FROM {1} WHERE {2} = '{3}'".format(attribute, table, key, id))
+	tuple_list = cursor.fetchall()
+	attribute_list = []
+	for tuple in tuple_list:
+		attribute = tuple[0]
+		attribute_list.append(attribute)
+	return attribute_list
+
+# General purpose function that returns all lists of attributes of a specified type in a table in relation to a series of keys, returns 2d list
+def getAttributeByKeylist(attribute, table, id, key_list):
+	attribute_list_list = []
+	for key in key_list:
+		attribute_list = getAttributesByKey(table, key, attribute, key)
+		attribute_list_list.append(attribute_list)
+	return attribute_list_list
+
+# Function for taking a 2d list and putting all elements in a single list
+def D2D(list2):
+	return_list = []
+	for list in list2:
+		for element in list:
+			return_list.append(element)
+	return return_list
+
+def getUsersFriends(uid):
+	return getAttributesByKey('Friends', 'user_id', 'frnd_id', uid)
+
+def getUserEmails(uid_list):
+	return D2D(getAttributeByKeylist('email', 'Users', 'user_id', uid_list))
+
+def getFriendsEmails(uid):
+	fids = getUsersFriends(uid)
+	friend_emails = getUserEmails(fids)
+	return friend_emails
+
+def getUserIdFromEmail(email):
+	cursor = conn.cursor()
+	cursor.execute("SELECT user_id  FROM Users WHERE email = '{0}'".format(email))
+	return cursor.fetchone()[0]
+
+def getFirstNameFromID(uid):
+	cursor = conn.cursor()
+	cursor.execute("SELECT fname  FROM Users WHERE email = '{0}'".format(uid))
+	return cursor.fetchone()[0]
+
+def getLastNameFromID(uid):
+	cursor = conn.cursor()
+	cursor.execute("SELECT lname  FROM Users WHERE email = '{0}'".format(uid))
+	return cursor.fetchone()[0]
+
+def getFullNameFromID(uid):
+	return getFirstNameFromID(uid) + ' ' + getLastNameFromID(uid)
+
+def getAlbumsFromID(uid):
+	cursor = conn.cursor()
+	cursor.execute("SELECT A_id FROM Albums WHERE Owner_id = '{0}'".format(uid))
+	tuple_list = cursor.fetchall
+	attribute_list = []
+	for tuple in tuple_list:
+		attribute = tuple[0]
+		attribute_list.append(attribute)
+	return attribute_list
+
+def isEmailUnique(email):
+	#use this to check if a email has already been registered
+	cursor = conn.cursor()
+	if cursor.execute("SELECT email  FROM Users WHERE email = '{0}'".format(email)):
+		#this means there are greater than zero entries with that email
+		return False
+	else:
+		return True
+	
+def isEmailRegistered(email):
+	#use this to check if a email has already been registered
+	cursor = conn.cursor()
+	if cursor.execute("SELECT email  FROM Users WHERE email = '{0}'".format(email)):
+		#this means there are greater than zero entries with that email
+		return True
+	else:
+		return False
+
+############################################################################################################################################
+# 	NAVIGATION AND FUNCTIONAL CODE	 #######################################################################################################
+############################################################################################################################################
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -116,7 +213,8 @@ def logout():
 def unauthorized_handler():
 	return render_template('unauth.html')
 
-#you can specify specific methods (GET/POST) in function header instead of inside the functions as seen earlier
+############################################################################################################################################
+
 @app.route("/register", methods=['GET'])
 def register():
 	return render_template('register.html', supress='True')
@@ -145,85 +243,29 @@ def register_user():
 		print("couldn't find all tokens")
 		return flask.redirect(flask.url_for('register'))
 	
-def getCurrentUid():
-	return getUserIdFromEmail(flask_login.current_user.id)
+###########################################################################################################################################
 
-def getUsersPhotos(uid):
-	cursor = conn.cursor()
-	cursor.execute("SELECT imgdata, picture_id, caption FROM Pictures WHERE user_id = '{0}'".format(uid))
-	return cursor.fetchall() #NOTE return a list of tuples, [(imgdata, pid, caption), ...]
+@app.route('/albums')
+@flask_login.login_required
+def myalbums():
+	return render_template('albums.html', name=getFullNameFromID(flask_login.current_user.id))
 
-def getUsersFriends(uid):
-	cursor = conn.cursor()
-	cursor.execute("SELECT frnd_id FROM friends WHERE user_id = '{0}'".format(uid))
-	frnd_id_tuples = cursor.fetchall() #NOTE return a list of tuples, [(imgdata, pid, caption), ...]
-	frnd_id_list = []
-	for frnd_id_tuple in frnd_id_tuples:
-		frnd_id = frnd_id_tuple[0]
-		frnd_id_list.append(frnd_id)
-	return frnd_id_list
-
-def getUserEmails(uid_list):
-	user_emails = []
-	for uid in uid_list:
-		cursor = conn.cursor()
-		cursor.execute("SELECT email FROM Users WHERE user_id = '{0}'".format(uid))
-		user_email_tuple = cursor.fetchall()
-		user_email = user_email_tuple[0][0]
-		user_emails.append(user_email)
-	return user_emails
-
-def getFriendsEmails(uid):
-	fids = getUsersFriends(uid)
-	friend_emails = getUserEmails(fids)
-	return friend_emails
-
-def getUserIdFromEmail(email):
-	cursor = conn.cursor()
-	cursor.execute("SELECT user_id  FROM Users WHERE email = '{0}'".format(email))
-	return cursor.fetchone()[0]
-
-def getFirstNameFromID(uid):
-	cursor = conn.cursor()
-	cursor.execute("SELECT fname  FROM Users WHERE email = '{0}'".format(uid))
-	return cursor.fetchone()[0]
-
-def getLastNameFromID(uid):
-	cursor = conn.cursor()
-	cursor.execute("SELECT lname  FROM Users WHERE email = '{0}'".format(uid))
-	return cursor.fetchone()[0]
-
-def getFullNameFromID(uid):
-	return getFirstNameFromID(uid) + ' ' + getLastNameFromID(uid)
-
-def isEmailUnique(email):
-	#use this to check if a email has already been registered
-	cursor = conn.cursor()
-	if cursor.execute("SELECT email  FROM Users WHERE email = '{0}'".format(email)):
-		#this means there are greater than zero entries with that email
-		return False
-	else:
-		return True
-	
-def isEmailRegistered(email):
-	#use this to check if a email has already been registered
-	cursor = conn.cursor()
-	if cursor.execute("SELECT email  FROM Users WHERE email = '{0}'".format(email)):
-		#this means there are greater than zero entries with that email
-		return True
-	else:
-		return False
-#end login code
+###########################################################################################################################################
 
 @app.route('/photos')
 @flask_login.login_required
 def photos():
-	return render_template('photos.html', name=getFullNameFromID(flask_login.current_user.id), photos=getUsersPhotos(getCurrentUid()), base64=base64)
+	return render_template('photos.html', name=getFullNameFromID(flask_login.current_user.id), 
+			photos=getUsersPhotos(getCurrentUid()), base64=base64)
+
+###########################################################################################################################################
 
 @app.route('/profile')
 @flask_login.login_required
 def protected():
 	return render_template('hello.html', name=getFullNameFromID(flask_login.current_user.id), message="Here's your profile")
+
+###########################################################################################################################################
 
 # Friends page route
 @app.route('/friends')
@@ -258,6 +300,8 @@ def add_friend():
 		print("couldn't find all tokens")
 		return render_template('addfriend.html', name=getFullNameFromID(flask_login.current_user.id), message="User not found, please try again.")
 
+###########################################################################################################################################
+
 # begin photo uploading code
 # photos uploaded using base64 encoding so they can be directly embeded in HTML
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -281,6 +325,7 @@ def upload_file():
 		return render_template('upload.html')
 #end photo uploading code
 
+###########################################################################################################################################
 
 #default page
 @app.route("/", methods=['GET'])
